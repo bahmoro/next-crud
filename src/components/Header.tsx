ee4ca7e40d5a723a8698dde3e805c4e1a3e90d32
bmoro <bmoro@grupoimagetech.com.br>
2021-11-04 14:14:54 -0400

    adição da logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+
 export default function Header() {
+
   return(
     <div className=" bg-pink-900 w-full h-20 ">
-
+        <div className='flex'>
+        <Image
+              priority
+              src="/MY.png"
+              height={80}
+              width={80}
+            />
+        </div>
     </div>
   )
 }
